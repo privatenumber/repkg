5dbe4d0a83b1d43fbd1a8f11b4570b238f1a393d
Hiroki Osame <hiroki.osame@gmail.com>
2020-04-24 00:43:56 -0400

    Cleaned up testing

--- test/build-module.test.js
+++ test/build-module.test.js
@@ -1,27 +1,21 @@
 const buildModule = require('../lib/build-module');
+const assert = require('assert');
 
 (async () => {
-	// const built = await buildModule('parse-ms@2.1.0');
-	// console.log({ built });
+	const modules = [
+		'parse-ms@2.1.0',
+		'vue@latest',
+		'pretty-ms@6.0.1',
+		'is-buffer@2.0.4',
+		'lodash@4.17.15/camelCase.js',
+		'@babel/code-frame',
+		// 'better-ajv-errors@0.6.7/lib/modern/index.js',
+		// 'uuid@3.2.1/v1'
+	];
 
-	// const built = await buildModule('vue@latest');
-	// console.log({ built });
+	for (const moduleName of modules) {
+		const built = await buildModule(moduleName);
+		assert(built.built.length > 100, `${moduleName} build is too small`);
+	}
 
-	// const built = await buildModule('pretty-ms@6.0.1');
-	// console.log({ built });
-
-	// const built = await buildModule('is-buffer@2.0.4');
-	// console.log({ built });
-
-	// const built = await buildModule('lodash@4.17.15/camelCase.js');
-	// console.log({ built });
-
-	// const built = await buildModule('@babel/code-frame');
-	// console.log({ built });
-
-	// const built = await buildModule('better-ajv-errors@0.6.7/lib/modern/index.js');
-	// console.log({ built });
-
-	// const built = await buildModule('uuid@3.2.1/v1');
-	// console.log({ built });
 })().catch(console.log);
