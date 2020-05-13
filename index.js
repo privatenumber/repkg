f8bb81fefa3ce2640f9319145faa421c5f99e03c
Hiroki Osame <osame@squareup.com>
2020-05-13 15:28:10 -0400

    Added dev server

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const serveLanding = require('./api/serve-landing');
+const buildModule = require('./api/build-module');
+
+function repkgRouter() {
+	const router = Router();
+
+	router.get('/', serveLanding);
+
+	router.get('/favicon.ico', (req, res) => {
+		res.end();
+	});
+
+	router.use((req, res, next) => {
+		res.header('Access-Control-Allow-Origin', '*');
+		next();
+	});
+
+	router.get('/:path(*)?', buildModule);
+
+	return router;
+}
+
+module.exports = repkgRouter;
